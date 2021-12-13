461921f9f3f8935e947c68e7abd7724f7cca29d3
kyungsun choi <kschoi0308@gmail.com>
2021-12-13 17:53:53 +0900

    feat: add signup ui

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Box, Button, Heading, Input, Grid } from "@chakra-ui/react";
 
 const Signup = () => {
-  return <div>회원가입</div>;
+  return (
+    <Box maxW="450px" mt={{ base: "20px", md: "100px" }} mx="auto">
+      <Box bg="white" borderWidth="1px" p={5}>
+        <Grid gap={5}>
+          <Heading fontSize="3xl">Signup</Heading>
+          <Input variant="flushed" type="text" placeholder="email" />
+          <Input variant="flushed" type="text" placeholder="name" />
+          <Input variant="flushed" type="text" placeholder="nickname" />
+          <Input variant="flushed" type="password" placeholder="password" />
+          <Button>join</Button>
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default Signup;
