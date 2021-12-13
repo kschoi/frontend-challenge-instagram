1cfbdd5413a49eb6c285d66a206c7ae2e343e337
kyungsun choi <kschoi0308@gmail.com>
2021-12-13 17:11:19 +0900

    feat: add router

--- src/components/posts/List.js
+++ src/components/posts/List.js
@@ -1,24 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Grid } from "@chakra-ui/react";
 import ListItem from "./ListItem";
-import { useSelector, useDispatch } from "react-redux";
-import { postsAction } from "../../store/posts";
 
-const List = () => {
-  // useSelector로 store의 state를 가져옵니다.
-  const { data, error, loading } = useSelector((state) => state.posts.posts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(postsAction.getPosts());
-  }, [dispatch]);
-
-  if (loading) return <div>로딩 중...</div>;
-  if (error) return <div>에러 발생...</div>;
-  if (!data) return null;
-
+const List = ({ data }) => {
   return (
-    <Grid gridTemplateColumns="repeat(3, 1fr)">
+    <Grid gridTemplateColumns="repeat(3, 1fr)" gap={1}>
       {data.map((item) => (
         <ListItem
           key={item.id}
