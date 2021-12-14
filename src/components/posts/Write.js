6200b9581e1d76b08365ae2c8706647929a28383
chloe <kschoi0308@gmail.com>
2021-12-14 21:31:46 +0900

    feat: write component layout

--- src/components/posts/Write.js
+++ src/components/posts/Write.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Box, Flex, Input, Textarea, Button } from "@chakra-ui/react";
 
 const Write = () => {
-  return <div>게시글 작성하기</div>;
+  return (
+    <Flex h="80vh" alignItems="stretch">
+      <Box>
+        <label htmlFor="img-upload">여기를 눌러 이미지를 등록하세요.</label>
+      </Box>
+      <Flex direction="column" h="100%" justifyContent="space-between">
+        <Input id="img-upload" type="file" />
+        <Textarea
+          placeholder="사진에 대한 설명을 적어주세요."
+          flex="1"
+        ></Textarea>
+        <Button>등록</Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Write;
