3d96783e736f7cb82cbce3b32d0b488fd1be7b49
chloe <kschoi0308@gmail.com>
2021-12-15 09:34:24 +0900

    feat: add write component UI

--- src/components/posts/Write.js
+++ src/components/posts/Write.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { Box, Flex, Input, Textarea, Button } from "@chakra-ui/react";
+import { Box, Grid, Textarea, Button } from "@chakra-ui/react";
 
 const Write = () => {
   return (
-    <Flex h="80vh" alignItems="stretch">
-      <Box>
-        <label htmlFor="img-upload">여기를 눌러 이미지를 등록하세요.</label>
+    <Grid h="450px" gridTemplateColumns="1fr 1fr" gap={4} mt={4}>
+      <Box overflow="hidden" borderWidth="1px" borderRadius="md">
+        <Box
+          as="label"
+          htmlFor="img-upload"
+          d="block"
+          h="full"
+          bg="gray.100"
+          color="gray.500"
+          textAlign="center"
+          lineHeight="448px"
+        >
+          여기를 눌러 이미지를 등록하세요.
+        </Box>
       </Box>
-      <Flex direction="column" h="100%" justifyContent="space-between">
-        <Input id="img-upload" type="file" />
+      <Grid h="full" gridTemplateRows="auto 1fr auto" gap={4}>
+        <input id="img-upload" type="file" />
         <Textarea
           placeholder="사진에 대한 설명을 적어주세요."
-          flex="1"
+          h="auto"
+          resize="none"
         ></Textarea>
-        <Button>등록</Button>
-      </Flex>
-    </Flex>
+        <Button colorScheme="red">등록</Button>
+      </Grid>
+    </Grid>
   );
 };
 
